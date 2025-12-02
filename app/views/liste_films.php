82e4cea0cd3f02d2b16daca776b434fcd671ad98
leclair92 <153756762+leclair92@users.noreply.github.com>
2025-12-01 20:10:47 -0500

    Merge de tout et ajustements visuels

--- app/views/liste_films.php
+++ app/views/liste_films.php
@@ -1,47 +1,35 @@
 <?php require __DIR__ . '/layouts/header.php'; ?>
+<div class="container py-5" data-bs-theme="light">
+    <div class="row bg-white" style="border-radius:0.5em">
+        <div class="col-lg-3 p-0 bg-secondary admin-dash-menu">
+            <?php require __DIR__ . '/layouts/dashboard-menu.php'; ?>
+        </div>
 
-<div class="dashboard-layout">
-    <div class="dashboard-content">
-
-        <h1>Liste des films</h1>
-        <table border="1" cellpadding="10" cellspacing="0">
-            <thead>
+        <div class="col-lg-9 p-4 admin-dash">
+            <h1 class="h2 mt-0">Films</h1>
+            <table border="1" class="mb-3">
                 <tr>
                     <th>Titre</th>
-                    <th>Réalisateur</th>
-                    <th>Genre</th>
                     <th>Année</th>
-                    <th>Actions</th>
+                    <th>Genre</th>
+                    <th class="text-end" style="max-width:100px;">Actions</th>
                 </tr>
-            </thead>
-            <tbody>
 
-            <?php if (!empty($films)): ?>
-                <?php foreach ($films as $film): ?>
+                <?php foreach ($films as $film) : ?>
                     <tr>
-                        <td><?= htmlspecialchars($film['titre']) ?></td>
-                        <td><?= htmlspecialchars($film['realisateur']) ?></td>
-                        <td><?= htmlspecialchars($film['genre_nom']) ?></td>
-                        <td><?= htmlspecialchars($film['annee_sortie']) ?></td>
-
-                        <td>
-                            <a href="index.php?action=edit_film&id=<?= $film['id'] ?>">Modifier</a>
-                            |
-                            <a href="index.php?action=delete_film&id=<?= $film['id'] ?>"
-                               onclick="return confirm('Êtes-vous sûre de vouloir supprimer ce film ?')">Supprimer</a>
+                        <td><span class="fw-bold"><?= htmlspecialchars($film['titre']) ?></span></td>
+                         <td><?= htmlspecialchars($film['annee_sortie']) ?></td>
+                         <td><?= htmlspecialchars($film['genre_nom']) ?></td>
+                        <td class="text-end">
+                            <a href="?action=edit_film&id=<?= $film['id'] ?>"><i class="bi bi-pencil-square" style="font-size: 1.3rem;"></i></a>
+                            &nbsp;<a href="?action=delete_film&id=<?= $film['id'] ?>"
+                            onclick="return confirm('Supprimer ?')"><i class="bi bi-trash3-fill" style="font-size: 1.3rem;"></i></a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="6">Aucun film trouvé.</td>
-                </tr>
-            <?php endif; ?>
-
-            </tbody>
-        </table>
-        <a href="index.php?action=add_film" class="btn-add">+ Ajouter un film</a>
+            </table>
+                 <a href="index.php?action=add_film" class="btn btn-secondary"><i class="bi bi-plus text-primary"s></i> Ajouter un film</a>
+        </div>
     </div>
 </div>
-
 <?php require __DIR__ . '/layouts/footer.php'; ?>
